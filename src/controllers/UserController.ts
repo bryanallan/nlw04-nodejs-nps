ba9d13b4406a5181883a57769ddfa7f1440f060d
bryanallan <bryanallan.h@gmail.com>
2021-02-28 11:19:18 -0300

    Next Level Week 04

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,47 @@
+import { Request, Response } from 'express';
+import { getCustomRepository } from 'typeorm';
+import { UsersRepository } from '../repositories/UsersRepository';
+import * as yup from 'yup';
+
+class UserController {
+    async create(request: Request, response: Response) {
+        const { name, email } = request.body;
+
+        const schema = yup.object().shape({
+            name: yup.string().required(),
+            email: yup.string().email().required()
+        });
+
+        try {
+            await schema.validate(request.body, { abortEarly: false });
+        } catch (err) {
+            return response.status(400).json({
+                error: err
+            });
+        }
+
+        const usersRepository = getCustomRepository(UsersRepository);
+
+        //SELECT * FROM USERS WHERE EMAIL = "EMAIL"
+        const userAlreadyExists = await usersRepository.findOne({
+            email
+        });
+
+        if (userAlreadyExists) {
+            return response.status(400).json({
+                error: "Usuário já existe.",
+            });
+        }
+
+        const user = usersRepository.create({
+            name,
+            email,
+        });
+
+        await usersRepository.save(user);
+
+        return response.status(201).json(user);
+    }
+}
+
+export { UserController };
